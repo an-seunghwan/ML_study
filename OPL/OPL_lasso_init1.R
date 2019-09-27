b149d04b2f45a7aad395cdd454aae70b5a6403e6
SeungHwan-AN <dpeltms79@naver.com>
2019-09-27 21:23:17 +0900

    update OPL_lasso_init 190927

--- OPL/OPL_lasso_init1.R
+++ OPL/OPL_lasso_init1.R
@@ -28,22 +28,10 @@ zero_pm = matrix(rep(0, p*m), nrow = p)
 zero_pp = matrix(rep(0, p*p), nrow = p)
 
 # get initial x
-# mat1 = cbind(I_pp, -I_pp)
-# mat2 = cbind(Aeq, Aeq)
-# mat3 = cbind(I_pp, I_pp)
-# x = Variable(2 * p)
-# constraints = list(mat2 %*% x == beq,
-#                    mat3 %*% x >= zero_p)
-# objective = Minimize(t(one_p) %*% mat1 %*% x)
-# problem = Problem(objective, constraints)
-# result = solve(problem)
-# result$getValue(x)
-
-# get initial x
 ##### solve
-# min sum(abs(beta))
-# s.t. Ax = b
-#     x >= 0
+# min   sum(abs(beta))
+# s.t.  Ax = b
+#       x >= 0
 #####
 x = Variable(p)
 constraints = list(Aeq %*% x == beq,
@@ -80,18 +68,21 @@ constraints = list(2 * P %*% x_init + D2 %*% target <= D3 %*% target, # stationa
 objective = Minimize(D1 %*% target)
 problem = Problem(objective, constraints)
 result = solve(problem)
-result$getValue(target)
+# result$getValue(target)
 max_lambda = result$getValue(target)[1]
 max_mu = result$getValue(target)[2:(1+m), , drop=F]
-max_nu = result$getValue(target)[(m+2):nrow(result$getValue(target)), ,drop=F]
+max_nu = result$getValue(target)[(m+2):nrow(result$getValue(target)), , drop=F]
 max_nu[max_nu < 1e-6] = 0.0 # for numerical stability 
 x_init; max_lambda; max_mu; max_nu
 
 # boundary check for active set
 idx1 = which(abs(2 * P %*% x_init + t(Aeq) %*% max_mu + max_nu - max_lambda) < 1e-6) # Right ineq part
 idx2 = which(abs(2 * P %*% x_init + t(Aeq) %*% max_mu + max_nu + max_lambda) < 1e-6) # Left ineq part
-
-
+# 1. lambda를 infinite부터 감소시키는 과정에서 boundary에 놓여있는 predictor들을 선택한 acitve set
+sort(union(idx1, idx2))
+# 2. 초기 x값이 0이 아닌 값들을 active set으로 선택하는 방법
+  # -> 이게 맞는 듯...?
+which(x_init != 0)
 
 
 
