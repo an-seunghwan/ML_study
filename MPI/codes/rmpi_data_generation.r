2550566b05aec788ac15039d8c8af0c0bfe7fd40
SeungHwan-AN <dpeltms79@naver.com>
2019-10-02 12:58:03 +0900

    repository organizing

--- MPI/codes/rmpi_data_generation.r
+++
@@ -1,7 +0,0 @@
-# 각 slave의 rank에 맞게 for문 안의 제어문을 수정하여 데이터를 생성한다
-for(i in 1:4){
-  x = matrix(rnorm(1000000), 100000, 10)
-  y = x %*% c(1,0,0,0,0,0,0,0,0,0) + rnorm(100000)
-  data = cbind(x, y)
-  write.csv(data, file = paste("/home/user/new_data",i,"_mpi.csv", sep=""),row.names = F)
-}
